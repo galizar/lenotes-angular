d9459d2704a4c3051f1d155b6175d6f5421e1948
Luis Galicia <luisgalicia8@hotmail.com>
2022-08-08 20:52:08 -0400

    added config for mapping of TS paths to api project

---
+++ apps/api/tsconfig-paths-bootstrap.js
@@ -0,0 +1,19 @@
+// this script configures tsconfig-paths to resolve the TS path aliases to their
+// correct relative path. this script is used by the start target of this project
+const tsConfigPaths = require('tsconfig-paths');
+
+const configLoadResult = tsConfigPaths.loadConfig('apps/api/tsconfig.app.json');
+
+const apiOutputRoot = 'dist/out-tsc';
+
+// each new path added to the root tsconfig (that this project depends on) 
+// must be added to this paths object too
+let paths = {
+	'@lenotes-ng/shared/model': [`${apiOutputRoot}/libs/shared/model/index.js`],
+	'@lenotes-ng/shared/assets': [`${apiOutputRoot}/libs/shared/assets/src/index.js`]
+};
+
+tsConfigPaths.register({
+	baseUrl: configLoadResult.absoluteBaseUrl,
+	paths 
+});
