651b465dc49e4c91bfc03a878ae9eb2a456a5b77
Luis Galicia <luisgalicia8@hotmail.com>
2022-08-31 21:20:51 -0400

    removed .env from source control

--- apps/client/src/app/groups/services/group.service.ts
+++ apps/client/src/app/groups/services/group.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, catchError } from 'rxjs';
 
 import { Group, ObjectMap } from '@lenotes-ng/model';
 import { EnvObject } from '../../../environments';
@@ -23,26 +23,57 @@ export class GroupService implements IGroupService {
 	 }
 
   create(dto: CreateGroupDto): Observable<number> {
-    return this.http.post<number>(this.env.GROUPS_API_ROOT, dto);
+    return this.http.post<number>(this.env.GROUPS_API_ROOT, dto)
+			.pipe(
+				catchError(this.handleError)
+			);
   }
 
   getAll(): Observable<ObjectMap<Group>> {
-    return this.http.get<ObjectMap<Group>>(this.env.GROUPS_API_ROOT);
+    return this.http.get<ObjectMap<Group>>(this.env.GROUPS_API_ROOT)
+			.pipe(
+				catchError(this.handleError)
+			);
   }
 
   get(id: number): Observable<Group['props']> {
-    return this.http.get<Group['props']>(`${this.env.GROUPS_API_ROOT}/${id}`);
+    return this.http.get<Group['props']>(`${this.env.GROUPS_API_ROOT}/${id}`)
+			.pipe(
+				catchError(this.handleError)
+			);
   }
 
   update(id: number, dto: UpdateGroupDto): Observable<object> {
-    return this.http.patch(`${this.env.GROUPS_API_ROOT}/updateOne/${id}`, dto);
+    return this.http.patch(`${this.env.GROUPS_API_ROOT}/updateOne/${id}`, dto)
+			.pipe(
+				catchError(this.handleError)
+			);
   }
 
 	batchUpdate(ids: number[], dto: UpdateGroupDto): Observable<object> {
-		return this.http.patch(`${this.env.GROUPS_API_ROOT}/batchUpdate`, {ids, dto});
+		return this.http.patch(`${this.env.GROUPS_API_ROOT}/batchUpdate`, {ids, dto})
+			.pipe(
+				catchError(this.handleError)
+			);
 	}
 
   delete(id: number): Observable<object> {
-    return this.http.delete(`${this.env.GROUPS_API_ROOT}/${id}`);
+    return this.http.delete(`${this.env.GROUPS_API_ROOT}/${id}`)
+			.pipe(
+				catchError(this.handleError)
+			);
   }
+
+	private handleError(error: HttpErrorResponse) {
+		if (error.status === 0) {
+			// client-side error
+			console.error('An error occurred', error.error);
+		} else {
+			// back-end error
+			console.log(`Server responded with error. Status: ${error.status}. Error body:`, error.error);
+		}
+
+		// user-facing error message
+		return throwError(() => new Error('An error occurred :/. Please try again later.'));
+	}
 }
--- apps/client/src/app/notes/services/note.service.ts
+++ apps/client/src/app/notes/services/note.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 
 import { ObjectMap, Note } from '@lenotes-ng/model';
 import { INoteService } from '../../interfaces';
@@ -22,30 +22,64 @@ export class NoteService implements INoteService {
 		}
 
   create(dto: CreateNoteDto): Observable<number> {
-    return this.http.post<number>(this.env.NOTES_API_ROOT, dto);
+    return this.http.post<number>(this.env.NOTES_API_ROOT, dto)
+			.pipe(
+				catchError(this.handleError)
+			);
   }
 
   getAll(): Observable<ObjectMap<Note>> {
-    return this.http.get<ObjectMap<Note>>(this.env.NOTES_API_ROOT);
+    return this.http.get<ObjectMap<Note>>(this.env.NOTES_API_ROOT)
+			.pipe(
+				catchError(this.handleError)
+			);
   }
 
   get(id: number): Observable<Note['props']> {
-    return this.http.get<Note['props']>(`${this.env.NOTES_API_ROOT}/${id}`);
+    return this.http.get<Note['props']>(`${this.env.NOTES_API_ROOT}/${id}`)
+			.pipe(
+				catchError(this.handleError)
+			);
   }
 
 	getInGroup(groupId: number): Observable<ObjectMap<Note>> {
-		return this.http.get<ObjectMap<Note>>(`${this.env.NOTES_API_ROOT}/getInGroup/${groupId}`);
+		return this.http.get<ObjectMap<Note>>(`${this.env.NOTES_API_ROOT}/getInGroup/${groupId}`)
+			.pipe(
+				catchError(this.handleError)
+			);
 	}
 
 	update(id: number, dto: UpdateNoteDto) {
-		return this.http.patch(`${this.env.NOTES_API_ROOT}/updateOne/${id}`, dto);
+		return this.http.patch(`${this.env.NOTES_API_ROOT}/updateOne/${id}`, dto)
+			.pipe(
+				catchError(this.handleError)
+			);
 	}
 
 	batchUpdate(ids: number[], dto: UpdateNoteDto) {
-		return this.http.patch(`${this.env.NOTES_API_ROOT}/batchUpdate`, {ids, subDto: dto});
+		return this.http.patch(`${this.env.NOTES_API_ROOT}/batchUpdate`, {ids, subDto: dto})
+			.pipe(
+				catchError(this.handleError)
+			);
 	}
 
   delete(id: number): Observable<object> {
-    return this.http.delete(`${this.env.NOTES_API_ROOT}/${id}`);
+    return this.http.delete(`${this.env.NOTES_API_ROOT}/${id}`)
+			.pipe(
+				catchError(this.handleError)	
+			);
   }
+
+	private handleError(error: HttpErrorResponse) {
+		if (error.status === 0) {
+			// client-side error
+			console.error('An error occurred', error.error);
+		} else {
+			// back-end error
+			console.log(`Server responded with error. Status: ${error.status}. Error body:`, error.error);
+		}
+
+		// user-facing error message
+		return throwError(() => new Error('An error occurred :/. Please try again later.'));
+	}
 }
