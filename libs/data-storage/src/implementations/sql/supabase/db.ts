c19403dab50e864dc3f3c71801ae61b7ca3fe6a9
Luis Galicia <luisgalicia8@hotmail.com>
2022-09-07 17:33:27 -0400

    improved tsconfig-paths bootstrap

--- libs/data-storage/src/implementations/sql/supabase/db.ts
+++ libs/data-storage/src/implementations/sql/supabase/db.ts
@@ -1,6 +1,19 @@
 import { createClient } from '@supabase/supabase-js';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL; 
 const supabaseKey = process.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl!, supabaseKey!);
+
+(async () => {
+	const {user, session, error} = 
+		await supabase.auth.signUp({email: 'lecrunchic@gmail.com', password: '123456'});
+
+	if (error) {
+		console.error(error);
+	}
+
+
+})();
