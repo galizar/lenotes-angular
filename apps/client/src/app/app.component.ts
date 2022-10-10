3d45a492e3d8a03a14f72c691f2fe0ae1991fe07
galizar <luisgalicia8@hotmail.com>
2022-10-10 00:42:37 -0400

    added drop to restore functionality

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -42,21 +42,43 @@ export class AppComponent {
 		const noteString = event.dataTransfer?.getData('Note');
 		const groupString = event.dataTransfer?.getData('Group');
 
-		if (noteString) {
-			const note = JSON.parse(noteString) as Note;
-
-			if (confirm('Are you sure you want to trash this note'))
-				this.noteStateService.update(note.id, {isTrashed: true});
-
-		} else if (groupString) { 
-			const group = JSON.parse(groupString) as Group;
-
-			if (confirm('Are you sure you want to trash this group? This will trash all notes within too.'))
-				this.groupStateService.update(group.id, {isTrashed: true});
-				this.noteStateService.trashInGroups([group.id]);
-		}
+		this.handleDrop('trash', noteString, groupString);
 	}
 
 	dropToRestore(event: DragEvent) {
+		event.preventDefault();
+		const noteString = event.dataTransfer?.getData('Note');
+		const groupString = event.dataTransfer?.getData('Group');
+
+		this.handleDrop('restore', noteString, groupString);
+	}
+
+	handleDrop(
+		op: 'trash' | 'restore', 
+		noteString?: string, 
+		groupString?: string
+	) {
+
+		const isTrashed = op === 'trash' ? true : false;
+		if (noteString) {
+			const note = JSON.parse(noteString) as Note;
+
+			if (confirm(`Are you sure you want to ${op} this note`)) {
+				this.noteStateService.update(note.id, { isTrashed });
+			}
+
+		} else if (groupString) {
+			const group = JSON.parse(groupString) as Group;
+
+			if (confirm(`Are you sure you want to ${op} this group? This will ${op} all notes within too.`)) {
+				this.groupStateService.update(group.id, { isTrashed });
+
+				if (isTrashed) {
+					this.noteStateService.trashInGroups([group.id]);
+				} else {
+					this.noteStateService.restoreInGroups([group.id]);
+				}
+			}
+		}
 	}
 }
